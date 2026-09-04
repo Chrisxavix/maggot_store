2696bbe35860d9adc039c36157fdd0fb480ce6e2
Chrisxavix <chrischillogallid64@outlook.com>
2026-09-03 23:56:59 -0500

    feat(clases): carpetas y archivos base

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import { MaggotShop } from './MaggotShop'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    
+    <MaggotShop></MaggotShop>
   </StrictMode>,
 )
